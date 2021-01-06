1910e0677a1ede99483930b107603d211caa7db4
benjieben <knightkrypto7@gmail.com>
2021-01-06 17:06:15 +0800

    Added/Modified Components

---
+++ src/components/Navbar/config.js
@@ -0,0 +1,9 @@
+const NavBarConfigs = [
+  {name: "Logo", styles: []}
+  {name: "Home", styles: []},
+  {name: "About Me", styles: []},
+  {name: "Projects", styles: []}, 
+  {name: "Contact Me", styles: []}
+];
+  
+export default NavBarConfigs;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React, {Component, Fragment} from "react";
+import "./index.css";
+import NavBarConfig from "./config.js";
+
+class NavBar extends Component {
+  constructor(props){
+    super(props);
+  }
+  
+  render(){
+    return (
+      <div className="nav-container">
+      </div>
+      );
+  }
+}
