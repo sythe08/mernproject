d2de6016b0bfd5dc6a1e4835ccd20342e62d69cc
benjieben <knightkrypto7@gmail.com>
2021-01-14 10:30:07 +0800

    added react-icons package

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,18 +1,14 @@
 import React, {Component} from "react";
 import "./index.css";
 
-class Hero extends Component{
-    constructor(props){
-        super(props);
-    }
-
-    render(){
-        return (
-            <div className="hero">
-                {props.children}
-            </div>
-        )
-    }
+function Hero({}){
+  return (
+    <>
+      <div className"hero-section">
+        Hero
+      </div>
+    </>
+  )
 }
 
 export default Hero;
