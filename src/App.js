a3b7a6e5e3b38ed74056cb92824b693baf046d2b
unknown <knightkrypto7@gmail.com>
2021-01-19 13:23:29 +0800

    added footer component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import NavBar from "./components/Navbar/";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 import "./index.css";
 
 function App() {
   return (
     <>
       <NavBar/>
-      <Home/>
-      <Projects/>
+      {/*<Home/>
+      <Projects/>*/}
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./index.css";
+
+function Footer(){
+
+    return (
+        <div className="footer">
+            <div className="footer-logo"></div>
+            <div className="footer-content">
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
